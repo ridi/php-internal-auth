f1ec9c8ff2354002b37cf89729712649952b91e5
김정근 <snorlax@ridi.com>
2020-02-26 13:36:06 +0900

    미사용 상수 정리

--- lib/Constant/AccessTokenConstant.php
+++ lib/Constant/AccessTokenConstant.php
@@ -1,10 +1,7 @@
 <?php declare(strict_types=1);
-namespace Ridibooks\OAuth2\Constant;
+namespace Ridibooks\InternalAuth\Constant;
 
 class AccessTokenConstant
 {
-    const ACCESS_TOKEN_INFO_KEY = 'access_token_info';
-    const ACCESS_TOKEN_COOKIE_KEY = 'ridi-at';
-
-    const DEFAULT_EXPIRE_MARGIN = 60 * 5;   // seconds
+    const AUTHORIZATION_HEADER_KEY = 'authorization';
 }
--- lib/Constant/JwkConstant.php
+++ lib/Constant/JwkConstant.php
@@ -1,12 +1,13 @@
 <?php declare(strict_types=1);
 
-namespace Ridibooks\OAuth2\Constant;
+namespace Ridibooks\InternalAuth\Constant;
 
 class JwkConstant
 {
     const JWK_EXPIRATION_SEC = 300;
     const RSA = 'RSA';
     const SIG = 'sig';
-    const KEYS = 'keys';
     const KID = 'kid';
+
+    const RESPONSE_KEY = 'key';
 }
